d34a4d156c9659026e5b57812ff0f14dc65f2dcf
823042332@qq.com <823042332@qq.com>
2025-06-23 15:05:18 +0800

    New version:0.1.2 完成迁移和修整

---
+++ src/designerz/behavior/__init__.py
@@ -0,0 +1,20 @@
+# 　　其中有分为：
+# 　　算法封装：模板方法、策略、命令模式
+# 　　对象去耦：中介、观察者模式
+# 　　抽象集合：迭代器模式
+# 　　行为扩展：访问者、责任链模式
+# 　　对象状态：状态模式
+# 　　解释器模式
+
+
+
+# 　　行为扩展：责任链模式
+
+from .watcher import Watcher
+from .steg import Strategy
+from .state import State
+from .order import Order
+from .mediator import Mediator
+from .fwz import Visitor
+from .explan import Explain
+
